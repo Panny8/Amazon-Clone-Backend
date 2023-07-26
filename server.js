e956f5436f464c34643ce2fd4acda76304f46fcc
Panny8 <111778724+Panny8@users.noreply.github.com>
2023-07-26 03:48:05 +0100

    Finshed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config()
 const ProductRoute = require('./routes/productRoutes');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 const app = express();
 app.use(express.json())
 
