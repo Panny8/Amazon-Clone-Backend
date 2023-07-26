dc3fd13476eb4b2922555516dcf5f5c900b8b478
Panny8 <111778724+Panny8@users.noreply.github.com>
2023-07-26 03:57:31 +0100

    Made Changes

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config()
 const ProductRoute = require('./routes/productRoutes');
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 const app = express();
 app.use(express.json())
 
 app.use('/api/products', ProductRoute);
-
+app.get('/', (req, res) => {
+    res.send("Amazon-Clone-Product")
+})
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
     console.log('connected to mongodb')
